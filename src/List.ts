8a88679c9c499c6fb166fb6e0b61fddfc692acd0
李瑞 <lirui.sep@gmail.com>
2024-06-20 16:02:32 +0800

    refactor : Simplify compose method - List example

--- src/List.ts
+++ src/List.ts
@@ -9,24 +9,35 @@ export class List {
 
     }
 
-    getSize(){
+    public getSize(){
         return this.size;
     }
 
-    add(element: Object) {
-
-        if (!this.readonly) {
-            let newSize = this.size + 1;
-            if (newSize > this.elements.length) {
-                let newElements = new Array(newSize * 2);
-                for (let i = 0; i < this.size; i++) {
-                    newElements[i] = this.elements[i];
-                }
-                this.elements = newElements;
-            }
-            this.elements[this.size++] = element;
+    public add(element: Object) {
+        if (this.readonly) {
+            return
         }
+        if (this.overCapacity()) {
+            this.grow();
+        }
+        this.addElement(element);
 
+    }
+
+    private addElement(element: Object) {
+        this.elements[this.size++] = element;
+    }
+
+    private grow() {
+        let newElements = new Array((this.size + 1) * 2);
+        for (let i = 0; i < this.size; i++) {
+            newElements[i] = this.elements[i];
+        }
+        this.elements = newElements;
+    }
+
+    private overCapacity() {
+        return this.size + 1 > this.elements.length;
     }
 }
 export default List;
